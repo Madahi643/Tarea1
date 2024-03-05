111205ea7af5c22197f383208aecc0fd94fa7097
Madahi643 <2009345380101@ingenieria.usac.edu.gt>
2024-03-05 13:53:03 -0600

    Agregamos

--- Loops.py
+++ Loops.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     n = int(input())
     if n>=1 and n<=20:
         for i in range(n):
-            if i!=n: Prueba1
-                a=i*i Prueba 2
-                print(a) Prueba3
+            if i!=n: Prueba1 Pruebas
+                a=i*i Prueba 2 Pruebas
+                print(a) Prueba3 Pruebas
