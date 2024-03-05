c406848561f76e033104a69badaa0006e0119a7a
Madahi643 <2009345380101@ingenieria.usac.edu.gt>
2024-03-05 13:57:26 -0600

    Ultima act

--- Loops.py
+++ Loops.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     n = int(input())
     if n>=1 and n<=20:
         for i in range(n):
-            if i!=n: Prueba1 Pruebas
-                a=i*i Prueba 2 Pruebas
-                print(a) Prueba3 Pruebas
+            Prueba1 Pruebas
+              Prueba 2 Pruebas
+                Prueba3 Pruebas
