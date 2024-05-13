258072813247d76528a8f93f7da0ce1517e7fa6d
Madahi643 <2009345380101@ingenieria.usac.edu.gt>
2024-05-13 11:39:26 -0600

    Prueba

---
+++ String_Split_and_Join copy.py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+     line = line.split(" ")
+     c = "-".join(line)
+     return c
+     
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
+    Prueba
